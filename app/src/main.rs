5a84dc8bcdab2a2a6f009c368559aff8f8c0a3b9
tktcorporation <tktcorporation.go@gmail.com>
2020-10-15 21:31:27 +0900

    use curl

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,20 @@
 use curl::easy::Easy;
+use std::io::{stdout, Write, stdin};
 
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+
+        stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+    let mut easy = Easy::new();
+    easy.url("https://www.rust-lang.org/").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    println!("{}", easy.response_code().unwrap());
 }
