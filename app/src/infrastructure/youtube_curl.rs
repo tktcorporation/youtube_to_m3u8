f77eb89c629bdbe6c8d22d65bb9e2a7983fd351d
tkt <tktcorporation.go@gmail.com>
2020-10-16 22:01:12 +0900

    refactor

--- app/src/infrastructure/youtube_curl.rs
+++ app/src/infrastructure/youtube_curl.rs
@@ -1,32 +1,38 @@
-use curl::easy::Easy;
-use std::io::{stdout, Write};
+use crate::domain;
+use domain::types::m3u8_url;
 
-pub fn get_video_info() -> u32 {
-    let mut easy = Easy::new();
-    easy.url("https://www.rust-lang.org/").unwrap();
-    easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
-        Ok(data.len())
-    })
-    .unwrap();
-    easy.perform().unwrap();
-    match easy.response_code() {
-        Ok(info) => info,
-        Err(error) => panic!("It fail to fetch a movie info: {}", error),
-    }
+pub async fn get_m3u8() -> m3u8_url::M3U8url {
+    m3u8_url::parse(&request("rvkxtVkvawc").await)
 }
 
-pub fn curl_sample() -> u32 {
-    let mut easy = Easy::new();
-    easy.url("https://www.rust-lang.org/").unwrap();
-    easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
-        Ok(data.len())
-    })
-    .unwrap();
-    easy.perform().unwrap();
-    match easy.response_code() {
-        Ok(info) => info,
-        Err(error) => panic!("It fail to fetch a movie info: {}", error),
+async fn request(id: &str) -> String {
+    // let mut url = String::from("https://www.youtube.com/get_video_info?video_id=");
+    // url.push_str(id);
+    let client = reqwest::Client::new();
+    let res = client.get("https://www.youtube.com/get_video_info?video_id=rvkxtVkvawc")
+        .header(reqwest::header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15").send().await.unwrap();
+    res.text().await.unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::infrastructure::youtube_curl;
+
+    #[tokio::test]
+    async fn it_request() {
+        let url = youtube_curl::request("rvkxtVkvawc");
+        assert_eq!(
+            "https%3A%2F%2Fmanifest.googlevideo.com/xxxxxxxxxxxxxxxxxx.m3u8",
+            url.await
+        );
+    }
+
+    #[tokio::test]
+    async fn it_get_m3u8() {
+        let url = youtube_curl::get_m3u8();
+        assert_eq!(
+            "https%3A%2F%2Fmanifest.googlevideo.com/xxxxxxxxxxxxxxxxxx.m3u8",
+            url.await.get_url()
+        );
     }
 }
