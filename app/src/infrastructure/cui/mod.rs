c5f8d0eb3f09c9054e37916cbd1cf37c8bcdedc7
tkt <tktcorporation.go@gmail.com>
2020-10-17 04:21:39 +0900

    receive id as a command line args

--- app/src/infrastructure/cui/mod.rs
+++ app/src/infrastructure/cui/mod.rs
@@ -1,12 +1,19 @@
 use crate::domain::youtube;
 use std::io::stdin;
 
-pub fn receive_youtube_id_from_user() -> youtube::video::id::Id {
+pub fn receive_youtube_id_from_user_by_stdin() -> youtube::video::id::Id {
     let mut youtube_id = String::new();
     println!("please enter an id of youtube video: ");
     stdin()
         .read_line(&mut youtube_id)
         .expect("Failed to read line");
     println!("input: \"{}\"", youtube_id);
+    youtube::video::id::new(&youtube_id.trim())
+}
+
+pub fn receive_youtube_id_from_user_by_args() -> youtube::video::id::Id {
+    let args: Vec<String> = std::env::args().collect();
+    let youtube_id = &args[1];
+    println!("youtube_id: \"{}\"", youtube_id);
     youtube::video::id::new(&youtube_id.trim())
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -5,7 +5,7 @@ mod infrastructure;
 
 #[tokio::main]
 async fn main() {
-    let youtube_id = cui::receive_youtube_id_from_user();
+    let youtube_id = cui::receive_youtube_id_from_user_by_args();
     let info = request::youtube::video::info::request(&youtube_id);
     println!("{}", info.await.extract_m3u8_url().url_decode());
 }
