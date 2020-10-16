a5b87ae7cb6bcb62ddd7e3f5d1bfd0c34ac2e8a8
tkt <tktcorporation.go@gmail.com>
2020-10-16 23:11:40 +0900

    impl refactored mods to main

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,15 +1,11 @@
+use infrastructure::cui;
 use infrastructure::youtube_curl;
-
 mod domain;
 mod infrastructure;
 
 #[tokio::main]
 async fn main() {
-    // let mut youtube_id = String::new();
-    // stdin()
-    //     .read_line(&mut youtube_id)
-    //     .expect("Failed to read line");
-
-    let info = youtube_curl::get_m3u8();
+    let youtube_id = cui::receive_youtube_id_from_user();
+    let info = youtube_curl::get_m3u8(&youtube_id);
     println!("{}", info.await.get_url());
 }
