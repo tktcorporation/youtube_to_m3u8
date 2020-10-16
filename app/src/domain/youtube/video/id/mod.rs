2b4af09ac92cb69c537ebd89ec7be931acc41eba
tkt <tktcorporation.go@gmail.com>
2020-10-17 00:25:45 +0900

    add a domain of youtube video id

---
+++ app/src/domain/youtube/video/id/mod.rs
@@ -0,0 +1,13 @@
+pub struct Id {
+    value: String,
+}
+pub fn new(st: &str) -> Id {
+    Id {
+        value: st.to_string(),
+    }
+}
+impl Id {
+    pub fn to_string(&self) -> String {
+        self.value.clone()
+    }
+}
