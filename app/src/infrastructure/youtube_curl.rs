32e936c3503ce39bf072aa66c9e1738af0f0ae20
tkt <tktcorporation.go@gmail.com>
2020-10-16 23:11:08 +0900

    use nw separated mods

--- app/src/infrastructure/youtube_curl.rs
+++ app/src/infrastructure/youtube_curl.rs
@@ -1,15 +1,16 @@
 use crate::domain;
 use domain::types::m3u8_url;
+use domain::types::youtube;
 
-pub async fn get_m3u8() -> m3u8_url::M3U8url {
-    m3u8_url::parse(&request("rvkxtVkvawc").await)
+pub async fn get_m3u8(id: &youtube::video::id::Id) -> m3u8_url::M3U8url {
+    m3u8_url::parse(&request(id).await)
 }
 
-async fn request(id: &str) -> String {
-    // let mut url = String::from("https://www.youtube.com/get_video_info?video_id=");
-    // url.push_str(id);
+async fn request(id: &youtube::video::id::Id) -> String {
+    let mut url = String::from("https://www.youtube.com/get_video_info?video_id=");
+    url.push_str(&id.to_string());
     let client = reqwest::Client::new();
-    let res = client.get("https://www.youtube.com/get_video_info?video_id=rvkxtVkvawc")
+    let res = client.get(&url)
         .header(reqwest::header::USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15").send().await.unwrap();
     res.text().await.unwrap()
 }
