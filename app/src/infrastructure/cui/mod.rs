66273537eb25c11b74fbfd23c7af40263395ad58
tkt <tktcorporation.go@gmail.com>
2020-10-16 23:10:17 +0900

    add a new mod for receiving user input

---
+++ app/src/infrastructure/cui/mod.rs
@@ -0,0 +1,10 @@
+use super::super::domain::types::youtube;
+use std::io::stdin;
+pub fn receive_youtube_id_from_user() -> youtube::video::id::Id {
+    let mut youtube_id = String::new();
+    println!("please enter an id of youtube video: ");
+    stdin()
+        .read_line(&mut youtube_id)
+        .expect("Failed to read line");
+    youtube::video::id::new(&youtube_id)
+}
