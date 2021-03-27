9126266d8e9afcbe0e677a989f64665091168f4f
tktcorporation <tktcorporation.go@gmail.com>
2021-03-28 04:06:24 +0900

    :+1: format result message

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ mod infrastructure;
 #[tokio::main]
 async fn main() {
     let youtube_id = cui::receive_youtube_id_from_user_by_args();
-    let info = request::youtube::video::info::request(&youtube_id).await.unwrap();
-    println!("{}", info.extract_m3u8_url().url_decode());
+    let info = request::youtube::video::info::request(&youtube_id).await;
+    let result = match info {
+        Ok(info) => Ok(info.extract_m3u8_url().url_decode()),
+        Err(err) => Err(err),
+    };
+    match result {
+        Ok(result) => println!("result: {}", result),
+        Err(err) => println!("error: {}", err),
+    }
 }
